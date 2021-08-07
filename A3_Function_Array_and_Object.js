87b8dbcc0887debc2a0347a18943d67846681d5a
JOYCE <joyce820309@gmail.com>
2021-08-07 12:06:06 +0800

    create A3_Function_Array_and_Object.js

---
+++ A3_Function_Array_and_Object.js
@@ -0,0 +1,30 @@
+function avg(data) {
+    // your code here
+    let items = data.products
+    let sum = 0
+    for (let i = 0; i < items.length; i++ ) {
+        
+        sum += items[i]['price']
+    }
+    return sum/ items.length
+}
+avg({
+    size: 3,
+    products: [
+        {
+            name: "Product 1",
+            price: 100
+        },
+        {
+            name: "Product 2",
+            price: 700
+        },
+        {
+            name: "Product 3",
+            price: 250
+        }
+    ]
+}); // show the average price of all products
+
+avg(avg)
+
